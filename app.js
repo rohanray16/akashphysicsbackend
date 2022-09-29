b05eb1324d8844b7075bbab549e97cdb79566209
rohanray@16 <rohanandmail@gmail.com>
2022-09-29 11:50:16 +0530

    4th heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false })); // get post data
+app.use(express.urlencoded({ extended: false })); // get post dat
 
 app.use("/api", apiRouter);
 
